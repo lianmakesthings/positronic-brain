cd64f07e95fcdc9eb8ed73c0fed4c4a76615331f
Chimney42 <draggili@gmail.com>
2016-03-06 23:29:10 +0100

    [REFACTORING] move helper methods to where it's needed

--- src/main.js
+++ src/main.js
@@ -1,30 +1,3 @@
 var marketValueParser = require('./marketValueParser');
 
-function cartesianProduct(a) { // a = array of array
-    var i, j, l, m, a1, o = [];
-    if (!a || a.length == 0) return a;
-
-    a1 = a.splice(0,1);
-    a = cartesianProduct(a);
-    for (i = 0, l = a1[0].length; i < l; i++) {
-        if (a && a.length) for (j = 0, m = a.length; j < m; j++)
-            o.push([a1[0][i]].concat(a[j]));
-        else
-            o.push([a1[0][i]]);
-    }
-    return o;
-}
-
-var dates = [];
-
-var years = ['2016'];
-var months = ['02'];
-var days = ['15'];
-
-dates.push.apply(dates, cartesianProduct([years, months, days]));
-
-dates = dates.map(function(el) {
-    return el.join('-');
-});
-
-marketValueParser.run(dates);
+marketValueParser.run();
--- src/marketValueParser.js
+++ src/marketValueParser.js
@@ -6,8 +6,34 @@ var store = require('./dataStore');
 
 var linkTemplate = 'http://www.transfermarkt.de/1-bundesliga/marktwerteverein/wettbewerb/L1/plus/?stichtag=%s';
 
+function cartesianProduct(a) { // a = array of array
+    var i, j, l, m, a1, o = [];
+    if (!a || a.length == 0) return a;
+
+    a1 = a.splice(0,1);
+    a = cartesianProduct(a);
+    for (i = 0, l = a1[0].length; i < l; i++) {
+        if (a && a.length) for (j = 0, m = a.length; j < m; j++)
+            o.push([a1[0][i]].concat(a[j]));
+        else
+            o.push([a1[0][i]]);
+    }
+    return o;
+}
+
 module.exports = {
-    run : function(dates) {
+    run : function() {
+        var dates = [];
+
+        var years = ['2014'];
+        var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+        var days = ['01', '15'];
+
+        dates.push.apply(dates, cartesianProduct([years, months, days]));
+
+        dates = dates.map(function(el) {
+            return el.join('-');
+        });
         async.map(dates, function (date, next) {
             var url = format(linkTemplate, date);
             request(url, function (err, response, body) {
